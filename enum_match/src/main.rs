51511f543472d3b1d195807cab5216f461b203df
Devisono <devison_santos@yahoo.com.br>
2026-02-19 23:31:31 -0300

    enum match

--- enum_match/src/main.rs
+++ enum_match/src/main.rs
@@ -27,4 +27,20 @@ fn main() {
     value_in_cents(Coin::Quater(UsState::Alabama));
     value_in_cents(Coin::Quater(UsState::Alaska));
     value_in_cents(Coin::Penny);
+
+    // match padrão
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("the value of six is: {six:?}");
+    let none = plus_one(None);
+    println!("the value of variable none: {none:?}")
 }
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
