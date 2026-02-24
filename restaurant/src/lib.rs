5c4caed5c73ef4831819a991094c2c1c89ed837e
Devisono <devison_santos@yahoo.com.br>
2026-02-23 23:39:46 -0300

    public enum

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -13,6 +13,8 @@ mod tests {
     }
 }
 
+fn deliver_order () {}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -29,6 +31,34 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
 
 pub fn eat_at_restaurant() {
     // absolut path
@@ -36,4 +66,12 @@ pub fn eat_at_restaurant() {
 
     // relative path
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+    
 }
